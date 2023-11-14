6012b25e92ed74d4bd6ebe781fac1339115d0d57
Jose-Manuel18 <103284630+Jose-Manuel18@users.noreply.github.com>
2023-11-14 12:57:31 -0400

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,86 @@
-import { StyleSheet } from 'react-native';
+import { Alert, Button, StyleSheet, TextInput } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-
+import { CardField, useConfirmPayment } from "@stripe/stripe-react-native";
+import { Details } from "@stripe/stripe-react-native/lib/typescript/src/types/components/CardFieldInput";
+import { useState } from "react";
+import { View } from "../../components/Themed";
+const API_URL = "http://192.168.0.126:3000";
 export default function TabOneScreen() {
+  const [email, setEmail] = useState<string>("");
+  const [cardDetails, setCardDetails] = useState<Details | null>(null);
+  const { confirmPayment, loading } = useConfirmPayment();
+
+  const fetchPaymentIntentClientSecret = async () => {
+    const response = await fetch(`${API_URL}/create-payment-intent`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
+    });
+
+    const { clientSecret, error } = await response.json();
+    return { clientSecret, error };
+  };
+
+  const handlePayPress = async () => {
+    if (!cardDetails?.complete || !email) {
+      Alert.alert("Error", "Please enter complete Card and email information");
+      return;
+    }
+    const billingDetails = {
+      email: email,
+    };
+
+    try {
+      const { clientSecret, error } = await fetchPaymentIntentClientSecret();
+      if (error) {
+        Alert.alert("Error", error);
+        return;
+      } else {
+        const { paymentIntent, error } = await confirmPayment(clientSecret, {
+          paymentMethodType: "Card",
+          paymentMethodData: { billingDetails: billingDetails },
+        });
+        if (error) {
+          Alert.alert("Error", error.message);
+        } else if (paymentIntent) {
+          Alert.alert("Success", paymentIntent.status);
+          console.log("Success", paymentIntent);
+        }
+      }
+    } catch (error) {
+      console.log("maybe here ", (error as Error).message);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        placeholder="Email"
+        placeholderTextColor="gray"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={email}
+        onChangeText={(value) => {
+          setEmail(value);
+        }}
+      />
+      <CardField
+        cardStyle={styles.card}
+        style={styles.cardContainer}
+        postalCodeEnabled={true}
+        placeholders={{
+          number: "4242 4242 4242 4242",
+        }}
+        onCardChange={(cardDetails) => {
+          setCardDetails(cardDetails);
+        }}
+      />
+
+      <Button title="Pay" onPress={handlePayPress} disabled={loading} />
     </View>
   );
 }
@@ -16,16 +88,23 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "burlywood",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  card: { backgroundColor: "#ccc" },
+  cardContainer: {
+    height: 50,
+    marginVertical: 30,
+    width: "80%",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,23 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,7 +25,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -46,11 +51,19 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <StripeProvider
+      publishableKey={
+        "pk_test_51MlKm2AKKjIUPD3I4yQOkYJyiseTqBBviG3qS7m5TCDxFXP3TxePNlf3t4Cy2ikIfzBQEjteMfb07LFkyHOikxGy006SWJ3mKM"
+      }
+      // urlScheme="http://localhost:3000"
+      // merchantIdentifier="merchant.com.tap2pay"
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+    </StripeProvider>
   );
 }
