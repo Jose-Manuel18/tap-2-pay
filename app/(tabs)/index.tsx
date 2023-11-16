533cdf3e049f847b44ca088fdf1f4b87ed0180c3
Jose-Manuel18 <103284630+Jose-Manuel18@users.noreply.github.com>
2023-11-16 15:44:50 -0400

    apple pay is working right now

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,110 +1,107 @@
 import { Alert, StyleSheet } from "react-native";
 
 import {
+  AddToWalletButton,
+  Constants,
+  GooglePayCardToken,
   PlatformPay,
-  PlatformPayButton,
+  canAddCardToWallet,
   confirmPlatformPayPayment,
   isPlatformPaySupported,
 } from "@stripe/stripe-react-native";
 import { useEffect, useState } from "react";
+import Stripe from "stripe";
+// import AddToGooglePayPNG from "../../assets/images/en_badge_web_generic.png";
 import { View } from "../../components/Themed";
+
 const API_URL = "http://192.168.0.126:3000";
+const ISSUING_CARD_ID = "ic_1ODAgtAKKjIUPD3IARaWQcnM";
+
+type CustomString = string | null;
 export default function TabOneScreen() {
   const [clientSecret, setClientSecret] = useState<string>("");
-  // const [email, setEmail] = useState<string>("");
-  // const [cardDetails, setCardDetails] = useState<Details | null>(null);
-  // const { confirmPayment, loading } = useConfirmPayment();
+  const [key, setKey] = useState({});
+  const [card, setCard] = useState<Stripe.Issuing.Card>();
+  const [showAddToWalletButton, setShowAddToWalletButton] = useState(false);
+  const [androidCardToken, setAndroidCardToken] =
+    useState<null | GooglePayCardToken>(null);
 
-  // const fetchPaymentIntentClientSecret = async () => {
-  //   const response = await fetch(`${API_URL}/create-payment-intent`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ email: email }),
-  //   });
+  useEffect(() => {
+    fetchEphemeralKey();
+    fetchIssuingCard();
+  }, []);
+  const checkIfCanAddCard = async (card: Stripe.Issuing.Card) => {
+    const { canAddCard, details, error } = await canAddCardToWallet({
+      primaryAccountIdentifier: card?.wallets
+        ?.primary_account_identifier as CustomString,
+      cardLastFour: card?.last4 as string,
+    });
 
-  //   const { clientSecret, error } = await response.json();
-  //   return { clientSecret, error };
+    if (error) {
+      Alert.alert(error.code, error.message);
+    } else {
+      setShowAddToWalletButton(canAddCard);
+      if (
+        details?.token?.status === "TOKEN_STATE_NEEDS_IDENTITY_VERIFICATION"
+      ) {
+        setAndroidCardToken(details.token);
+      }
+    }
+  };
+  // const canAddCard = async () => {
+  //   // See above
   // };
 
-  // const handlePayPress = async () => {
-  //   if (!cardDetails?.complete || !email) {
-  //     Alert.alert("Error", "Please enter complete Card and email information");
-  //     return;
-  //   }
-  //   const billingDetails = {
-  //     email: email,
-  //   };
+  const fetchIssuingCard = async () => {
+    console.log("fetchIssuingCard");
+    const response = await fetch(`${API_URL}/issuing-card`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ISSUING_CARD_ID,
+      }),
+    });
+    console.log("response", response);
+    const card = await response.json();
+    console.log("card", card);
+    await checkIfCanAddCard(card);
 
-  //   try {
-  //     const { clientSecret, error } = await fetchPaymentIntentClientSecret();
-  //     if (error) {
-  //       Alert.alert("Error", error);
-  //       return;
-  //     } else {
-  //       const { paymentIntent, error } = await confirmPayment(clientSecret, {
-  //         paymentMethodType: "Card",
-  //         paymentMethodData: { billingDetails: billingDetails },
-  //       });
-  //       if (error) {
-  //         Alert.alert("Error", error.message);
-  //       } else if (paymentIntent) {
-  //         Alert.alert("Success", paymentIntent.status);
-  //         console.log("Success", paymentIntent);
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.log("maybe here ", (error as Error).message);
-  //   }
-  // };
-  // const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    if (!card.wallets.apple_pay.eligible) {
+      console.log("apple_pay.eligible");
+      // Do not show <AddToWalletButton /> component on iOS. See card.wallets.apple_pay.ineligible_reason for details
+    } else if (!card.wallets.google_pay.eligible) {
+      console.log("google_pay.eligible");
+      // Do not show <AddToWalletButton /> component on Android. See card.wallets.google_pay.ineligible_reason for details
+    } else {
+      setCard(card);
+    }
+  };
+
+  const fetchEphemeralKey = async () => {
+    const response = await fetch(`${API_URL}/ephemeral-key`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ISSUING_CARD_ID,
+        API_VERSION: Constants.API_VERSIONS.ISSUING,
+      }),
+    });
+    const myKey = await response.json();
+    setKey(myKey);
+  };
+
   const [loading, setLoading] = useState(false);
-
-  // const fetchPaymentSheetParams = async () => {};
-
-  // const initializePaymentSheet = async () => {
-  //   try {
-  //     const { paymentIntent, ephemeralKey, customer, publishableKey } =
-  //       await fetchPaymentSheetParams();
-
-  //     const { error } = await initPaymentSheet({
-  //       merchantDisplayName: "Example, Inc.",
-  //       customerId: customer,
-  //       customerEphemeralKeySecret: ephemeralKey,
-  //       paymentIntentClientSecret: paymentIntent,
-  //       applePay: {
-  //         merchantCountryCode: "US",
-  //       },
-  //       allowsDelayedPaymentMethods: true,
-  //       defaultBillingDetails: {
-  //         name: "Jane Doe",
-  //       },
-  //     });
-
-  //     if (!error) {
-  //       setLoading(true);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error initializing payment sheet:", error);
-  //   }
-  // };
-
-  // const openPaymentSheet = async () => {
-  //   const { error } = await presentPaymentSheet();
-
-  //   if (error) {
-  //     Alert.alert(`Error code: ${error.code}`, error.message);
-  //   } else {
-  //     Alert.alert("Success", "Your order is confirmed!");
-  //   }
-  // };
 
   const Setup = async () => {
     if (!(await isPlatformPaySupported())) {
       Alert.alert("Error", "Platform Pay is not supported on this device");
       return;
     }
+
     const response = await fetch(`${API_URL}/payment-sheet`, {
       method: "POST",
       headers: {
@@ -121,6 +118,13 @@ export default function TabOneScreen() {
   const buy = async () => {
     setLoading(false);
     const { error } = await confirmPlatformPayPayment(clientSecret, {
+      googlePay: {
+        currencyCode: "USD",
+        merchantCountryCode: "US",
+        merchantName: "Example Merchant",
+        testEnv: true,
+        isEmailRequired: false,
+      },
       applePay: {
         cartItems: [
           {
@@ -141,18 +145,53 @@ export default function TabOneScreen() {
       Alert.alert("Success", "Your order is confirmed!");
     }
   };
-
-  useEffect(() => {
-    Setup();
-  }, []);
+  const createCardHolder = async () => {
+    const response = await fetch(`${API_URL}/create-card/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+  };
 
   return (
-    <View style={styles.container}>
-      <PlatformPayButton
-        style={styles.payButton}
-        onPress={buy}
-        disabled={!loading}
-      />
+    // <View style={styles.container}>
+    //   <PlatformPayButton
+    //     style={styles.payButton}
+    //     onPress={buy}
+    //     disabled={!loading}
+    //     type={PlatformPay.ButtonType.Pay}
+    //   />
+    //   <Button title="Card Holder" onPress={createCardHolder} />
+    // </View>
+    <View>
+      {showAddToWalletButton && (
+        <AddToWalletButton
+          token={androidCardToken}
+          androidAssetSource={require("../../assets/images/en_badge_web_generic.png")}
+          testEnv={true}
+          style={styles.payButton}
+          iOSButtonStyle="onLightBackground"
+          cardDetails={{
+            name: card?.cardholder?.name as string,
+            primaryAccountIdentifier: card?.wallets
+              ?.primary_account_identifier as CustomString, // This can be null, but should still always be passed. Failing to pass the primaryAccountIdentifier can result in a failure to provision the card.
+            lastFour: card?.last4,
+            description: "Added by Stripe",
+          }}
+          ephemeralKey={key}
+          onComplete={({ error }) => {
+            Alert.alert(
+              error ? error.code : "Success",
+              error
+                ? error.message
+                : "Card was successfully added to the wallet.",
+            );
+          }}
+        />
+      )}
     </View>
   );
 }
@@ -178,12 +217,11 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     width: "80%",
   },
+
   payButton: {
-    width: 200,
+    width: "50%",
     height: 50,
-    backgroundColor: "#ccc",
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: 60,
+    alignSelf: "center",
   },
 });
--- server/index.ts
+++ server/index.ts
@@ -14,6 +14,73 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+app.post("/issuing-card", async (req, res) => {
+  const card = await stripe.issuing.cards
+    .retrieve(req.body.ISSUING_CARD_ID)
+    .catch((error) => {
+      console.log(error);
+      return null;
+    });
+  res.json(card);
+});
+
+app.post("/create-ephemeral-key", async (req, res) => {
+  let key = await stripe.ephemeralKeys
+    .create(
+      { issuing_card: req.body.ISSUING_CARD_ID },
+      { apiVersion: req.body.API_VERSION },
+    )
+    .catch((error) => {
+      console.log(error);
+      return null;
+    });
+  res.json(key);
+});
+app.post("/create-card/:carlHolderId", async (req, res) => {
+  const card = await stripe.issuing.cards
+    .create({
+      cardholder: req.params.carlHolderId,
+      currency: "usd",
+      type: "virtual",
+    })
+    .catch((error) => {
+      console.log(error);
+      return null;
+    });
+  res.json(card);
+});
+app.post("/create-card-holder", async (req, res) => {
+  const cardholder = await stripe.issuing.cardholders
+    .create({
+      name: "Jenny Rosen",
+      email: "jenny.rosen@example.com",
+      phone_number: "+18008675309",
+      status: "active",
+      type: "individual",
+      individual: {
+        first_name: "Jenny",
+        last_name: "Rosen",
+        dob: { day: 1, month: 11, year: 1981 },
+      },
+      billing: {
+        address: {
+          line1: "123 Main Street",
+          city: "San Francisco",
+          state: "CA",
+          postal_code: "94111",
+          country: "US",
+        },
+      },
+    })
+    .catch((error) => {
+      console.log(error);
+
+      return null;
+    });
+
+  res.json(cardholder);
+});
+
 app.post("/payment-sheet", async (req, res) => {
   const {
     email = "jmmg185@gmail.com",
@@ -37,6 +104,7 @@ app.post("/payment-sheet", async (req, res) => {
         request_three_d_secure: request_three_d_secure || "automatic",
       },
     },
+
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
@@ -51,3 +119,4 @@ app.post("/payment-sheet", async (req, res) => {
       "pk_test_51MlKm2AKKjIUPD3I4yQOkYJyiseTqBBviG3qS7m5TCDxFXP3TxePNlf3t4Cy2ikIfzBQEjteMfb07LFkyHOikxGy006SWJ3mKM",
   });
 });
+//ich_1ODAaXAKKjIUPD3INXxCnCrL
